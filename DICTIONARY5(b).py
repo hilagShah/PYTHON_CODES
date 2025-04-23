471279388feae0377e8159118015c5e5d43640a1
hilagShah <hilagshahop@gmail.com>
2025-04-24 00:07:38 +0530

    Update and rename Dictionary - Q5(b).py to DICTIONARY5(b).py

--- DICTIONARY5(b).py
+++ DICTIONARY5(b).py
@@ -1,9 +1,9 @@
 grocery_prices = {'apple' : 160,'banana' : 40,'orange' : 100,'pineapple' : 60,'mango' : 200}
 
 customers = {
-'veer' : {'apple' : 2,'banana' : 3,'orange' : 1,'pineapple' : 1,'mango' : 10},
-'hemang' : {'apple' : 3,'banana' : 9,'orange' : 1,'pineapple' : 4,'mango' : 20},
-'shivam' : {'apple' : 6,'banana' : 7,'orange' : 1,'pineapple' : 8,'mango' : 10}
+'XYZ' : {'apple' : 2,'banana' : 3,'orange' : 1,'pineapple' : 1,'mango' : 10},
+'YZX' : {'apple' : 3,'banana' : 9,'orange' : 1,'pineapple' : 4,'mango' : 20},
+'ZXY' : {'apple' : 6,'banana' : 7,'orange' : 1,'pineapple' : 8,'mango' : 10}
 }
 
 
